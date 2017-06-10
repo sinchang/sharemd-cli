fc155a5e0ff2351788e9b6409c55994880e11e78
sinchang <sinchangwen@gmail.com>
2017-06-10 12:34:01 +0800

    lint code style

--- test.js
+++ test.js
@@ -1,29 +1,32 @@
-const coffee = require('coffee')
-const path = require('path')
+const coffee = require(`coffee`)
+const path = require(`path`)
 
-describe('sharemd', function() {
-  it('add', function(done) {
-    coffee.fork(path.join(__dirname,'./index.js'), ['readme.md'])
-    .expect('stdout', /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
-    .end(done)
+describe(`sharemd`, () => {
+  it(`add`, done => {
+    coffee
+      .fork(path.join(__dirname, `./index.js`), [`readme.md`])
+      .expect(`stdout`, /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
+      .end(done)
   })
 
-  it('update', function(done) {
-    coffee.fork(path.join(__dirname, './index.js'), ['readme.md', '--i', 'h8UfScR2bx84KknJ'])
-    .expect('stdout', /h8UfScR2bx84KknJ/)
-    .end(done)
+  it(`update`, done => {
+    coffee
+      .fork(path.join(__dirname, `./index.js`), [`readme.md`, `--i`, `h8UfScR2bx84KknJ`])
+      .expect(`stdout`, /h8UfScR2bx84KknJ/)
+      .end(done)
   })
 
-  it('set style', function(done) {
-    coffee.fork(path.join(__dirname, './index.js'), ['readme.md', '--s', 'hexo'])
-    .expect('stdout', /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
-    .end(done)
+  it(`set style`, done => {
+    coffee
+      .fork(path.join(__dirname, `./index.js`), [`readme.md`, `--s`, `hexo`])
+      .expect(`stdout`, /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
+      .end(done)
   })
 
-   it('set title', function(done) {
-    coffee.fork(path.join(__dirname, './index.js'), ['readme.md', '--t', 'hello md'])
-    .expect('stdout', /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
-    .end(done)
+  it(`set title`, done => {
+    coffee
+      .fork(path.join(__dirname, `./index.js`), [`readme.md`, `--t`, `hello md`])
+      .expect(`stdout`, /Your markdown online url is: http:\/\/api.sinchang.me\/markdown\//)
+      .end(done)
   })
-
 })
